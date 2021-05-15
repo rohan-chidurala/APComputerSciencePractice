f61f58c7f9b31129ae8bfb810554cee9a1d901ca
ROHAN CHIDURALA <rohan.chidura@gmail.com>
2021-05-15 04:30:33 -0700

    Create Expressions1.java

---
+++ Chapter 2/Expressions1.java
@@ -0,0 +1,31 @@
+/*
+Self-Check 2.2: expressions1 (parts a-f)
+Trace the evaluation of the following expressions, and give their resulting values.
+
+2 + 3 * 4 - 6
+
+14 / 7 * 2 + 30 / 5 + 1
+
+(12 + 3) / 4 * 2
+
+(238 % 10 + 3) % 7
+
+(18 - 7) * (43 % 10)
+
+2 + 19 % 5 - (11 * (5 / 2))
+*/
+
+/*ANSWER:
+2 + 3 * 4 - 6
+8
+14 / 7 * 2 + 30 / 5 + 1
+11
+(12 + 3) / 4 * 2
+6
+(238 % 10 + 3) % 7
+4
+(18 - 7) * (43 % 10)
+33
+2 + 19 % 5 - (11 * (5 / 2))
+-16
+*/
